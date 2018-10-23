f91a6774300359cf3b2aaf1c91869ab5b1458a44
Tasima <re.anderson617@gmail.com>
2018-10-23 15:10:49 -0400

    oops I didn't make that connection variable an if/else statement. that should fix it

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,24 @@
 require("dotenv").config();
 
 // Set up MySQL connection.
+console.log(process.env.JAWSDB_URL)
 var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-}
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: "burger_db"
-});
+} else {
+  connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: process.env.MYSQL_PASSWORD || "",
+      database: "burger_db"
+    }
+  });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
